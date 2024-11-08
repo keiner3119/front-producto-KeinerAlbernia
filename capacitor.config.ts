2bc275d4338c0325f9e51aa2a6ba40bde926a723
KeinerAlbernia <139978777+KeinerAlbernia@users.noreply.github.com>
2024-11-08 15:06:20 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'front-producto-keinerAlbernia',
+  webDir: 'www'
+};
+
+export default config;
